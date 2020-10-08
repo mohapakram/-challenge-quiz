4b22befd5aec5f9536929f4d03c4526cea81eeda
mohab akram <mohapmobile@gmail.com>
2020-10-08 11:16:01 +0200

    feat: add progress bar on top of the screen

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import ProgressBar from '../progressBar/ProgressBar'
 import './App.css'
 
+export default App
+
 function App () {
-  return <h1>Hello</h1>
+  return (
+    <>
+      <ProgressBar width='10%' />
+      <h1>Hello</h1>
+    </>
+  )
 }
 
-export default App
---
+++ src/components/progressBar/ProgressBar.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './ProgressBar.css';
+
+export default ({width})=> <div id="progress-bar" style={{width}}></div>;
