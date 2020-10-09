2a70ca0fc19eac986371bd25e2c2d2bbbd321086
mohab akram <mohapmobile@gmail.com>
2020-10-09 13:11:41 +0200

    refactor: add discribe block for calcProgress

--- src/utils/calculations/calculations.test.js
+++ src/utils/calculations/calculations.test.js
@@ -1,10 +1,12 @@
 import calculations from './calculations'
 
-it('successfully calculate the progress', () => {
-  const calcProgress = calculations.calcProgress
-  const currentQuestionIndex = 0
-  const questionsLength = 10
-  expect(calcProgress(currentQuestionIndex, questionsLength)).toEqual(10)
+describe('calcProgress', () => {
+  it('successfully calculate the progress', () => {
+    const calcProgress = calculations.calcProgress
+    const currentQuestionIndex = 0
+    const questionsLength = 10
+    expect(calcProgress(currentQuestionIndex, questionsLength)).toEqual(10)
+  })
 })
 
 describe('calcWrongAnswers', () => {
