84452311c56999602a79dbd8401485b7ef55af7a
mohab akram <mohapmobile@gmail.com>
2020-10-09 12:35:30 +0200

    style: add space between the score and the percentage

--- src/components/scoreBar/ScoreBar.js
+++ src/components/scoreBar/ScoreBar.js
@@ -8,8 +8,8 @@ function ScoreBar ({ scores }) {
 
   return (
     <div id='score-bar'>
-      <span className='score'>score:{score}%</span>
-      <span className='max-score'>max score:{maxScore}%</span>
+      <span className='score'>score: {score}%</span>
+      <span className='max-score'>max score: {maxScore}%</span>
       <div className='representation-bar'>
         <div id='represent-max-score' style={{ width: `${maxScore}%` }} />
         <div id='represent-score' style={{ width: `${score}%` }} />
