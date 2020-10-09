6f8fa22ae858f340be4d54c93fe093ea754fda4b
mohab akram <mohapmobile@gmail.com>
2020-10-09 12:22:18 +0200

    refactor: question component

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -11,16 +11,32 @@ function Question ({ question, position, goToNextQuestion }) {
   const { difficulty, category } = question
   const answers = [...question.incorrect_answers, question.correct_answer]
 
-  const validateAnswer = (answer) => {
-    setFeedback(answer === question.correct_answer ? 'correct answer!' : 'wrong answer..')
-    setIsCorrectAnswer(answer === question.correct_answer)
-  }
-
   const renderNextQuestion = (feedback) => {
     setFeedback(undefined)
     goToNextQuestion(isCorrectAnswer)
     setIsCorrectAnswer(undefined)
   }
+
+  const renderContent = () => <div className='content'>
+    {decodeString(question.question)}
+  </div>
+
+  const renderAnswers = () => (
+    <div className='answers-container'>
+      {answers.map((answer, i) => (
+        <button key={i} className={feedback ? colorAnswer(answer) : 'answer'} disabled={feedback} onClick={() => validateAnswer(answer)}>
+          {decodeString(answer)}
+        </button>
+      ))}
+    </div>
+  )
+
+  const validateAnswer = (answer) => {
+    setFeedback(answer === question.correct_answer ? 'correct answer!' : 'wrong answer..')
+    setIsCorrectAnswer(answer === question.correct_answer)
+  }
+
+  const colorAnswer = (answer) => answer === question.correct_answer ? 'answer correct' : 'answer incorrect'
 
   const renderFeedback = () => {
     return (
@@ -31,21 +47,11 @@ function Question ({ question, position, goToNextQuestion }) {
     )
   }
 
-  const colorAnswer = (answer) => answer === question.correct_answer ? 'answer correct' : 'answer incorrect'
-
   return (
     <div id='question'>
       <QuestionHeader difficulty={difficulty} category={category} position={position} />
-      <div className='content'>
-        {decodeString(question.question)}
-      </div>
-      <div className='answers-container'>
-        {answers.map((answer, i) => (
-          <button key={i} className={feedback ? colorAnswer(answer) : 'answer'} disabled={feedback} onClick={() => validateAnswer(answer)}>
-            {decodeString(answer)}
-          </button>
-        ))}
-      </div>
+      {renderContent()}
+      {renderAnswers()}
       {feedback && renderFeedback()}
     </div>
   )
