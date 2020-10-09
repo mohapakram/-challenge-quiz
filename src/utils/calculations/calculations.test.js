6727967458a0a63302cb49437bf6d682f5d9dfb4
mohab akram <mohapmobile@gmail.com>
2020-10-09 13:10:26 +0200

    refactor: group tests for calcWrongAnswers i

--- src/utils/calculations/calculations.test.js
+++ src/utils/calculations/calculations.test.js
@@ -7,18 +7,20 @@ it('successfully calculate the progress', () => {
   expect(calcProgress(currentQuestionIndex, questionsLength)).toEqual(10)
 })
 
-it('keep the wrong answers the same if the answer is right', () => {
-  const calcWrongAnswers = calculations.calcWrongAnswers
-  const isCorrectAnswer = true
-  const wrongAnswers = 1
-  expect(calcWrongAnswers(isCorrectAnswer, wrongAnswers)).toEqual(wrongAnswers)
-})
+describe('calcWrongAnswers', () => {
+  it('keep the wrong answers the same if the answer is right', () => {
+    const calcWrongAnswers = calculations.calcWrongAnswers
+    const isCorrectAnswer = true
+    const wrongAnswers = 1
+    expect(calcWrongAnswers(isCorrectAnswer, wrongAnswers)).toEqual(wrongAnswers)
+  })
 
-it('increase the wrong answers if the answer was wrong', () => {
-  const calcWrongAnswers = calculations.calcWrongAnswers
-  const isCorrectAnswer = false
-  const wrongAnswers = 1
-  expect(calcWrongAnswers(isCorrectAnswer, wrongAnswers)).toEqual(wrongAnswers + 1)
+  it('increase the wrong answers if the answer was wrong', () => {
+    const calcWrongAnswers = calculations.calcWrongAnswers
+    const isCorrectAnswer = false
+    const wrongAnswers = 1
+    expect(calcWrongAnswers(isCorrectAnswer, wrongAnswers)).toEqual(wrongAnswers + 1)
+  })
 })
 
 describe('calcCorrectAnswers', () => {
