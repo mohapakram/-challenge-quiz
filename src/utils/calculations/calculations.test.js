d9c39479644619b39d0f4b75a53983f3942f6fa5
mohab akram <mohapmobile@gmail.com>
2020-10-09 13:00:34 +0200

    test: add test fs for calcWrongAnswers

--- src/utils/calculations/calculations.test.js
+++ src/utils/calculations/calculations.test.js
@@ -6,3 +6,17 @@ it('successfully calculate the progress', () => {
   const questionsLength = 10
   expect(calcProgress(currentQuestionIndex, questionsLength)).toEqual(10)
 })
+
+it('keep the wrong answers the same if the answer is right', () => {
+  const calcWrongAnswers = calculations.calcWrongAnswers
+  const isCorrectAnswer = true
+  const wrongAnswers = 1
+  expect(calcWrongAnswers(isCorrectAnswer, wrongAnswers)).toEqual(wrongAnswers)
+})
+
+it('increase the wrong answers if the answer was wrong', () => {
+  const calcWrongAnswers = calculations.calcWrongAnswers
+  const isCorrectAnswer = false
+  const wrongAnswers = 1
+  expect(calcWrongAnswers(isCorrectAnswer, wrongAnswers)).toEqual(wrongAnswers + 1)
+})
